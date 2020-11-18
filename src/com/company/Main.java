a95b00223a4ce70b7733eeb2554000aaf531b048
MrKartozka <mrkartozka@mail.ru>
2020-11-18 22:38:27 +0300

    Izmenenie

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,5 +6,6 @@ public class Main {
         System.out.println("Hello, World!");
         System.out.println("Second commit");
         System.out.println("Third commit");
+        System.out.println("Izmenenie");
     }
 }
